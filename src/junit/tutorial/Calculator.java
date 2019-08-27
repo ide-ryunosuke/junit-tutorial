5200fd65a596387061b58af176fdd541200dcdfd
ide-ryunosuke <r.i1996423@gmail.com>
2019-08-28 00:50:26 +0900

    08282019end

--- src/junit/tutorial/Calculator.java
+++ src/junit/tutorial/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
 	public int multiply(int x, int y) {
 		return x * y;
 	}
+	
 	public int divide(int x, int y) {
 		return x / y;
 	}
--- test/junit/tutorial/CalculatorTest.java
+++ test/junit/tutorial/CalculatorTest.java
@@ -8,10 +8,26 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void multiplyTest() {
+	public void multiplyTest1() {
 		Calculator calc = new Calculator();
 		int expected = 12;
 		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void multiplyTest2() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(4, 5);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void divideTest() {
+		Calculator calc = new Calculator();
+		int expected = 3;
+		int actual = calc.divide(12, 4);
 		assertThat(actual, is(expected));
 	}
 
