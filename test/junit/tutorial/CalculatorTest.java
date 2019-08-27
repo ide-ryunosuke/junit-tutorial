e41eb34cb9c1ae14df0794330c43d3bce5ad34f3
ide-ryunosuke <r.i1996423@gmail.com>
2019-08-28 00:21:59 +0900

    multiplyTest

---
+++ test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,18 @@
+package junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void multiplyTest() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+
+}
